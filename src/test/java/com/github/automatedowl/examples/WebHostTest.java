3cb26c335b82d0238653efdb32d045b964aa474b
Andi3z <gremo77t@gmail.com>
2019-09-09 16:38:22 +0300

    my 1st commit

--- src/test/java/com/github/automatedowl/examples/WebHostTest.java
+++ src/test/java/com/github/automatedowl/examples/WebHostTest.java
@@ -11,7 +11,7 @@ public class WebHostTest extends WebHostTestCase {
         getDriver().get(site88Page.getURL());
 
         // Type 'Hello World' to text box.
-        site88Page.getTextBox().sendKeys("Hello World");
+        site88Page.getTextBox().sendKeys("blablabla");
 
         // Wait before closing browser.
         waitBeforeClosingBrowser();
